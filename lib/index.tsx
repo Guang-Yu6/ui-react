b728986ff6d2dc20221233759a893805904ec7e2
Guang-Yu6 <everyting-day@foxmail.com>
2021-06-03 23:38:45 +0800

    css-dialog

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
+export {default as Icon} from './icon/icon';
 
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-};
+import './index.scss';
 
-ReactDOM.render(<div>
-  <Icon name='wx' onClick={fn}/>
-  <Icon name='zfb' onClick={fn}/>
-  <Icon name='bd' onClick={fn}/>
-</div>, document.querySelector('#root'));
